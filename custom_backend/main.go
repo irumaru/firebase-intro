22269e970d330ba0796071d502fb7e7b526a7489
irumaru <jibakomaorochi13@outlook.jp>
2024-02-18 05:13:03 +0000

    firestoreアクセスの追加

--- custom_backend/main.go
+++ custom_backend/main.go
@@ -38,6 +38,30 @@ func firebaseVerifyIDToken(ctx context.Context, app *firebase.App, idToken strin
 	return nil
 }
 
+func firestoreAccess(ctx context.Context, app *firebase.App) error {
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		return errors.Join(errors.New("error initializing Firestore client: "), err)
+	}
+	defer client.Close()
+
+	log.Println("Firestore client initialized successfully!")
+
+	// Add data
+	doc, result, err := client.Collection("users").Add(ctx, map[string]interface{}{
+		"resource_id": 10,
+		"file_id":     10,
+		"file_name":   "test.go",
+	})
+	if err != nil {
+		log.Fatalf("Failed adding alovelace: %v", err)
+	}
+
+	log.Printf("Firestore document added successfully: ID=%s, Time=%s\n", doc.ID, result.UpdateTime)
+
+	return nil
+}
+
 func main() {
 	ctx := context.Background()
 	app, err := firebaseInit(ctx)
@@ -45,8 +69,13 @@ func main() {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
 
-	// Use app
+	// Verify ID token
 	if err := firebaseVerifyIDToken(ctx, app, "idToken"); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Access Firestore
+	if err := firestoreAccess(ctx, app); err != nil {
 		log.Fatalln(err)
 	}
 }
