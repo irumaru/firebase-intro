515669f73d1b5c07ef78862983ab104a5b423f78
irumaru <jibakomaorochi13@outlook.jp>
2024-02-18 03:44:27 +0000

    コンテキスト伝搬&コード修正

--- custom_backend/main.go
+++ custom_backend/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log"
 
 	firebase "firebase.google.com/go/v4"
@@ -11,40 +10,43 @@ import (
 	"google.golang.org/api/option"
 )
 
-func firebaseInit() (*firebase.App, error) {
+func firebaseInit(ctx context.Context) (*firebase.App, error) {
 	opt := option.WithCredentialsFile("./secrets/intro-8f3f7-firebase-adminsdk-zqeta-dabd9b34fe.json")
-	app, err := firebase.NewApp(context.Background(), nil, opt)
+	app, err := firebase.NewApp(ctx, nil, opt)
 
 	if err != nil {
 		return nil, errors.Join(errors.New("error initializing app: "), err)
 	}
 
-	fmt.Println("Firebase app initialized successfully!")
+	log.Println("Firebase app initialized successfully!")
 
 	return app, nil
 }
 
-func firebaseVerifyIDToken(app *firebase.App, idToken string) (bool, error) {
-	client, err := app.Auth(context.Background())
+func firebaseVerifyIDToken(ctx context.Context, app *firebase.App, idToken string) error {
+	client, err := app.Auth(ctx)
 	if err != nil {
-		return false, errors.Join(errors.New("error getting Auth client: "), err)
+		return errors.Join(errors.New("error getting Auth client: "), err)
 	}
 
-	token, err := client.VerifyIDToken(context.Background(), idToken)
+	token, err := client.VerifyIDToken(ctx, idToken)
 	if err != nil {
-		return false, errors.Join(errors.New("error verifying ID token: "), err)
+		return errors.Join(errors.New("error verifying ID token: "), err)
 	}
 
 	log.Printf("Verified ID token: UID=%s\n", token.UID)
-	return true, nil
+	return nil
 }
 
 func main() {
-	app, err := firebaseInit()
+	ctx := context.Background()
+	app, err := firebaseInit(ctx)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
 
 	// Use app
-	firebaseVerifyIDToken(app, "idToken")
+	if err := firebaseVerifyIDToken(ctx, app, "idToken"); err != nil {
+		log.Fatalln(err)
+	}
 }
